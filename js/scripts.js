cba6f90ac20592dda081159d50676f547ec441ae
Vladimir <antonsimeonvladimir@gmail.com>
2018-01-31 19:34:39 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('.button-menu').click(function() {
 		$('body').toggleClass('menu-open');
 	});
+	$('.tobi-grid').click(function() {
+		$(this).toggleClass('active');
+		$('.mini-list').toggleClass('mini-list2');
+	});
 });
