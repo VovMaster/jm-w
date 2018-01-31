240680611331249b479facaa4af9e7d8e53df5f8
vladimir <vovmaster2@gmail.com>
2018-01-31 19:29:18 +0200

    fg

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	$('.button-menu').click(function() {
+	/*$('.button-menu').click(function() {
 		$('body').toggleClass('menu-open');
+	});*/
+	$('.accordion-heading').click(function() {
+		$(this).next().toggleClass('open');
+		$(this).children('.plus').toggleClass('close');
+		return false
 	});
 });
