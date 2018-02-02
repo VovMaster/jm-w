9aa65e818d89c1f80b313457d75d942cdeacf6a5
vladimir <vovmaster2@gmail.com>
2018-02-02 19:26:18 +0200

    hkjh

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 	/*$('.button-menu').click(function() {
 		$('body').toggleClass('menu-open');
 	});*/
+	$('.nav-btn').click(function() {
+		$('.menu-block').toggleClass('open');
+		return false
+	});
+	$('.nav-btn').click(function() {
+		$('body').toggleClass('body-bg');
+		return false
+	});
+	$('.tabs a').click(function() {
+		$(this).toggleClass('current');
+		return false
+	});
 	$('.accordion-heading').click(function() {
 		$(this).next().toggleClass('open');
 		$(this).children('.plus').toggleClass('close');
